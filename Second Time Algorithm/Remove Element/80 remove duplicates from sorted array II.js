273f769a2d2f1b7ffa609aa8e9820ae5082a5e8e
Mona <xiaomei.mona.he@gmail.com>
2025-06-18 14:15:04 +0800

    change file name

---
+++ Second Time Algorithm/Remove Element/80 remove duplicates from sorted array II.js
@@ -0,0 +1,16 @@
+nums = [1, 2, 3, 4, 5, 6]
+var removeDuplicates = function(nums) {
+    let i = nums.length-1;
+    if(i <= 2) return nums.length;
+    let k = 2;
+    for(i = 2; i<=nums.length-1; i++){
+        if(nums[i]!==nums[k-2]){
+            nums[k]=nums[i];
+            k++;
+        }
+    }
+    return k
+};
+
+var a = removeDuplicates(nums);
+console.log(a);
