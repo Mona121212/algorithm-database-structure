ce6d37b87a6f67decd4d9fed7540605cdc7de3af
Mona <xiaomei.mona.he@gmail.com>
2025-06-19 22:41:57 +0800

    feat rotate list

--- Second Time Algorithm/Rotate Element/61 rotate list.js
+++ Second Time Algorithm/Rotate Element/61 rotate list.js
@@ -1,6 +1,29 @@
 
-
-
 //make circle
 //find breakpoint
 // break circle
+
+var rotateRight = function(head, k) {
+    if(!head || !head.next || k === 0) return head;
+
+    let length = 1;
+    let current = head;
+    while(current.next) {
+        current = current.next;
+        length++;
+    }
+
+    k = k % length;
+    current.next = head;
+
+    let newTail = head;
+
+    for(let i = 0; i < length - 1 - k; i++) {
+        newTail = newTail.next;
+    }
+
+    let newHead = newTail.next;
+    newTail = null;
+
+    return newHead;
+}
