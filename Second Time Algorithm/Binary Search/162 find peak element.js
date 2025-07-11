2dc78cb2ce19619438a152533d1d3cf14883688b
Mona <xiaomei.mona.he@gmail.com>
2025-07-11 13:52:01 +0800

    feat binary search 35 and 162

---
+++ Second Time Algorithm/Binary Search/162 find peak element.js
@@ -0,0 +1,17 @@
+var findPeakElement = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    //terminate condition
+
+    while(left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if(nums[mid] > nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
