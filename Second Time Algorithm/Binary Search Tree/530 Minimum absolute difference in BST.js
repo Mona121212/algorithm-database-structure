9d30f58414f8eb629de59c838c0500deb3716062
Mona <xiaomei.mona.he@gmail.com>
2025-07-13 15:38:42 +0800

    feat binary search tree 530 minimum absolute difference in BST

---
+++ Second Time Algorithm/Binary Search Tree/530 Minimum absolute difference in BST.js
@@ -0,0 +1,18 @@
+var getMinimumDifference = function(root) {
+    // minDiff = node.val - prev; traversal order is left root right, first prev is null
+
+    let prev = null;
+    let minDiff = Infinity;
+
+    var isMin = function(node) {
+        if(!root) return;
+        isMin(node.left);
+
+        if(prev !== null) {
+            minDiff = Math.min(minDiff, node.val - prev);
+        }
+        prev = node.val; // the first node.val is left
+    }
+    isMin(root);
+    return minDiff;
+}
