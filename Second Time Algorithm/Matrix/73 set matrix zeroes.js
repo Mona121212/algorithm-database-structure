22a35469c9dc8896c418940cb5e43c0a9be45ef2
Mona <xiaomei.mona.he@gmail.com>
2025-07-21 12:11:32 +0800

    fix 73 matrix zeroes mistakes

--- Second Time Algorithm/Matrix/73 set matrix zeroes.js
+++ Second Time Algorithm/Matrix/73 set matrix zeroes.js
@@ -2,47 +2,49 @@ var setZeroes = function(matrix) {
     let m = matrix.length;
     let n = matrix[0].length;
 
-    let firstRowZero = false;
     let firstColZero = false;
+    let firstRowZero = false;
 
-    for(let i = 0; i < n; i++) {
+    for(let i = 0; i < m; i++) {
         if(matrix[i][0] === 0) {
-            firstRowZero = true;
-            break;
-        }
-    }
-
-    for(let j = 0; j < m; j++) {
-        if(matrix[0][j] === 0) {
             firstColZero = true;
             break;
         }
     }
 
-    for(let i = 1; i < n; i++) {
-        for(let j = 1; j < m; j++) {
+    for(let j = 0; j < n; j++) {
+        if(matrix[0][j] === 0) {
+            firstRowZero = true;
+            break;
+        }
+    }
+
+    for(let i = 1; i < m; i++) {
+        for(let j = 1; j < n; j++) {
             if(matrix[i][j] === 0) {
                 matrix[i][0] = 0;
                 matrix[0][j] = 0;
             }
         }
     }
-    for(let i = 1; i < n; i++) {
-        for(let j = 1; j < m; j++) {
-            if(matrix[i][0]=== 0 || matrix[0][j]=== 0) {
-                matrix[i][j]=0;
+    
+    for(let i = 1; i < m; i++) {
+        for(let j = 1; j < n; j++) {
+            if(matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j]= 0;
             }
         }
     }
-     if(firstColZero) {
-        for (let i = 0; i < m; i++) {
+
+    for(let i = 0; i < m; i++) {
+        if(firstColZero = true) {
             matrix[i][0] = 0;
         }
     }
 
-    if(firstRowZero) {
-        for (let j = 0; j < n; j++) {
-            matrix[0][j] = 0;
+    for(let j = 0; j < n; j++) {
+        if(firstRowZero = true) {
+            matrix[0][j] = 0
         }
     }
 }
