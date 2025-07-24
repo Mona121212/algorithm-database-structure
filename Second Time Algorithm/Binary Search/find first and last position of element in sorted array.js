34624a52c847127fdd97f454701055d50b6e2ee2
Mona <xiaomei.mona.he@gmail.com>
2025-07-24 20:52:17 +0800

    feat: 34 find first and last position element in sorted array

---
+++ Second Time Algorithm/Binary Search/find first and last position of element in sorted array.js
@@ -0,0 +1,41 @@
+var searchRange = function(nums, target) {
+    function findLeft(nums, target) {
+        let left = 0;
+        let right = nums.length - 1;
+        let index = -1;
+
+        while(left <= right) {
+            let mid = Math.floor((left + right) / 2);
+
+            if(target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                if(nums[mid] === target) index = mid;
+                right = mid - 1;
+            }
+        }
+        return index;
+    }
+
+    function findRight(nums, target) {
+        let left = 0;
+        let right = nums.length - 1;
+        let index = -1;
+
+        while(left <= right) {
+            let mid = Math.floor((left + right) / 2);
+
+            if(target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                if(nums[mid] === target) index = mid
+                left = mid + 1;
+            }
+        }
+        return index;
+    }
+
+    const start = findLeft(nums, target);
+    const end = findRight(nums, target);
+    return [start, end];
+}
