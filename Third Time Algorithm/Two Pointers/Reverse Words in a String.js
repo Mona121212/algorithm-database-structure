abfa7ca9df17a23de2e48391a1591801816ced65
Mona <xiaomei.mona.he@gmail.com>
2025-08-29 23:30:49 -0600

    feat: two pointer-4

--- Third Time Algorithm/Two Pointers/Reverse Words in a String.js
+++ Third Time Algorithm/Two Pointers/Reverse Words in a String.js
@@ -1,8 +1,42 @@
-var reverseWords = function(s) {
-    let l = 0, r = s.length - 1;
-    while(l < r) {
-        [s[l], s[r]] = [s[r], s[l]];
-        l++;
-        r--;
+var reverserWords = function(s) {
+    const strArr = Array.from(s);
+
+    removeExtraSpaces(strArr);
+
+    reverse(strArr, 0, strArr.length - 1);
+
+    let start = 0;
+
+    for(let i = 0; i <= strArr.length; i++) {
+        if(start[i] === ' ' || i === strArr.length) {
+            reverse(strArr, start, i - 1);
+            start = i + 1;
+        }
+
+    }
+    return strArr.join('');
+
+    function removeExtraSpaces(strArr) {
+        let slowIndex = 0, fastIndex = 0;
+
+        // skip leading spaces
+        while(fastIndex < strArr.length ){
+            if(strArr[fastIndex] === ' ' && (fastIndex === 0 || strArr[fastIndex - 1] === ' ')) {
+                fastIndex++;
+            } else {
+                strArr[slowIndex] = strArr[fastIndex];
+                slowIndex++;
+                fastIndex++;
+            }       
+        }
+    }
+
+    function reverse(strArr, start, end) {
+        let left = start, right = end;
+        while(left < right) {
+            [strArr[left], strArr[right]] = [strArr[right], strArr[left]];
+            left++;
+            right--;
+        }
     }
 }
